e8480128455eacbf308fa8c04fa764ad4cd77b70
ervinizzm <61249211+ervinizzm@users.noreply.github.com>
2021-02-16 03:02:48 +0200

    {Sivustovalikko, sivuston alatunniste}

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Logo from './BC.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import {
+    Label,
+    Input,
+    Select,
+    Radio,
+    Checkbox,
+} from '@rebass/forms'
+import {Button, Form, Nav, Navbar} from "react-bootstrap";
+import {Box} from "@material-ui/core";
+import {Flex} from "rebass";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar style={{ backgroundColor: '#3139E5' }} variant="dark">
+                <Navbar.Brand href="#home">
+                    <img width="234px" height="30px" className="img-responsive" src={Logo}  alt="logo" />
+                </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/"><span className="material-icons d-inline-block align-top">savings</span> Home</Nav.Link>
+                    <Nav.Link href="/about"><span className="material-icons d-inline-block align-top">savings</span> About</Nav.Link>
+                    <Nav.Link href="/users">
+                        <span className="material-icons d-inline-block align-top">home</span> Users</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/"><span className="material-icons d-inline-block align-top">login</span> Kirjaudu sisään</Nav.Link>
+                    </Nav>
+                    <Button variant="outline-light" href="/users"><span className="material-icons d-inline-block align-top">person_add</span> Rekisteröidy</Button>
+                </Form>
+            </Navbar>
+            <div>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return (
+    <Router>
+        <div className="container">
+            <h2>Home</h2>
+        </div>
+        <footer className="new_footer_area">
+            <div className="new_footer_top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6">
+                            <ul>
+                                <li>
+                                    <Nav.Link href="/users"><span className="material-icons d-inline-block align-top">history</span> Historia</Nav.Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-3 col-md-6">
+                            <ul>
+                                <li>
+                                    <Nav.Link href="/users"><span className="material-icons d-inline-block align-top">star_rate</span> Tallennettu</Nav.Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer_bottom">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-sm-7">
+                            <p>© 2021 Bitcoin converter. Kaikki oikeudet pidätetään.</p>
+                        </div>
+                        <div className="col-lg-6 col-sm-5 text-right">
+                            <p>Made with ❤️</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </Router>
+    );
+}
+
+function About() {
+    return (
+        <Router>
+            <div className="container">
+                <h2>About</h2>
+            </div>
+        </Router>
+    );
+}
+
+function Users() {
+    return (
+        <Router>
+            <div className="container">
+                <h2>Users</h2>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
